e3cc55abb795bb03c7081ff5c5b15786ab109f43
leobar37 <usatloqueando@gmail.com>
2020-10-01 22:05:32 -0500

    update routes

--- src/classes/nodemailer.ts
+++ src/classes/nodemailer.ts
@@ -3,26 +3,26 @@ import handlebars from "handlebars";
 import { readFile, writeFile } from "fs";
 import { Nodemailer } from "../helpers/Nodemailer";
 import { readHtml } from "../helpers/helpers";
-
+import { join, resolve } from "path";
 export const sendEmailWithTemplate = async (data: {
   template: string;
   email: string;
   subject: string;
   data: any;
 }) => {
-  readHtml(
-    __dirname + `../../../public/emails/${data.template}.hbs`,
-    (err, html) => {
-      const template = handlebars.compile(html);
-      let htmlsend = template(data.data);
-      /// * send email
-      Nodemailer.emailSend(htmlsend, data.email, data.subject)
-        .then((res) => {
-          return res;
-        })
-        .catch((err) => {
-          return err;
-        });
-    }
-  );
+  const path = resolve(__dirname + `../../../public/emails/${data.template}.hbs`);
+  console.log(path);
+
+  readHtml(path, (err, html) => {
+    const template = handlebars.compile(html);
+    let htmlsend = template(data.data);
+    /// * send email
+    Nodemailer.emailSend(htmlsend, data.email, data.subject)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  });
 };
