6adbe7a83191da60432e06971bfc2c877a04f292
leobar37 <usatloqueando@gmail.com>
2020-10-02 16:40:38 -0500

    updater routes

--- src/globals/enviroment.ts
+++ src/globals/enviroment.ts
@@ -5,9 +5,9 @@ export const SECRET: string =
 export const EXPIRESTOKEN: string = process.env.EXPIRESTOKEN || "2h";
 export const SALTPASSWORD: number = Number(process.env.SALTPASSWORD) || 10;
 export const CLIENTIDPAYPAL =
-  "AUEEc8VanjbcryNbf57htXTtTF49Gg-PfDyd8UM1v3Jfcod7wkD00Rx6_XtMZk7Lf7YF5vVwlinhQ6YA";
+  "Ad9LKTak4sIpx0n8c-sYxbKbGklVKVT-K7htPY6ePqjjBslCeujkytIgWD4634YespisyWS5Lm4Vqnqv";
 export const CLIENTSECRETPAYPAL =
-  "ENr68wWDpA4Y2TqmWgVzAXYEk0XI4MR9sb24JP5cYxvL-8tWXN_LXKSXF0Ge3mOTXlv1oidzZOGxlEqh";
+  "EGffBosul4FW3t0Q-hlXySCt8qPUiIzzh-nqHDFzi7Z9dcsAS1TMLEPvXmcscpcbYX6eCKCc4PJLCqlu";
 
 export const HostFrotend = "lo";
 
